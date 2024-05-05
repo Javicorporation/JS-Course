5c8f462487572ab5e7a63e535384b336c07b7c23
Javicorporation <krchipullajavier@gmail.com>
2024-05-04 22:55:09 -0500

    prueba de proyecto 1

---
+++ Course 2 JS/Excercises/tarea 1.js
@@ -0,0 +1,16 @@
+// Math.round = redondea el numero decimal .5
+/*let numero = Math.round(38.22);
+
+let numero1 = Math.ceil(33.2);
+
+console.log(numero, numero1);*/
+
+let cadena = "hola que tal te va"; 
+
+console.log(cadena);
+console.log(cadena.length);
+console.log(cadena.split(0, 6));
+console.log(cadena.slice(0,5));
+console.log(cadena.substring(0,8));
+
+
---
+++ Course 2 JS/Excercises/tarea 2.js
@@ -0,0 +1,8 @@
+let nombre = 'pablo';
+let edad = 19;
+
+if (edad >= 18 && nombre === 'pablo') {
+    console.log("usted es mayor de edad")
+}else{
+    console.log("usted es un menor de edad")
+}
